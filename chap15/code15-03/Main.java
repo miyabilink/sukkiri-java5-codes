7e90e9951376e1077df3f33482dfd5511cdf1398
Takuma SUGAWARA <sugawara@miyabilink.jp>
2026-04-17 17:24:28 +0900

    4版の指摘を5版にも反映（コード15-3）

--- chap15/code15-03/Main.java
+++ chap15/code15-03/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   void main() {
-    String s1 = "Java programing";
+    String s1 = "Java programming";
     IO.println("文字列s1の4文字目以降は" + s1.substring(3));        // ⇒ a programing
     IO.println("文字列s1の4～8文字目以降は" + s1.substring(3, 8));  // ⇒ a pro
   }
